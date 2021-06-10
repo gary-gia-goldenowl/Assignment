536f3fc16680ece75ca06a81cef26f448bf5e95e
gary <gary-gia-goldenowl@gmail.com>
2021-06-10 12:26:35 +0700

    fix: asset

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-require("../../assets/stylesheets/application.scss");
+require("../../assets/stylesheets/application.css.scss");
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
