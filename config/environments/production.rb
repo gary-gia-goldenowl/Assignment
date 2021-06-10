1e29866ba36d3523fe3e7431a139ec76cda86403
gary <gary-gia-goldenowl@gmail.com>
2021-06-10 11:10:42 +0700

    test02

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,8 @@ Rails.application.configure do
   # these configuration options.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.digest = true
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
