49f37d709c17297ec474e6799faaf162a8b4db8f
gary <gary-gia-goldenowl@gmail.com>
2021-06-10 12:08:41 +0700

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  Rails.application.config.assets.precompile += %w[application.css.scss]
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.assets.check_precompiled_asset = false
